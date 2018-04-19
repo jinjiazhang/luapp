506e103ef60be8ca1b2a8caabca651c503ebb7a3
jinjiazhang <jinjiazh@qq.com>
2018-04-19 10:15:45 +0800

    log module

---
+++ protolog/protolog.cpp
@@ -0,0 +1,15 @@
+#include "protolog.h"
+#include <windows.h>
+#include <stdio.h>
+
+void proto_log(int level, const char* fmt, ...)
+{
+    va_list	args;
+    va_start(args, fmt);
+#ifdef _WIN32
+    _vprintf_p(fmt, args);
+#else
+    vprintf(format, args);
+#endif // _WIN32
+    va_end(args);
+}
