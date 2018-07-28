abfb5274cb25c2ec5dbfc8d9f5f01c700b4d9266
jinjiazhang <jinjiazh@qq.com>
2018-07-28 10:56:24 +0800

    submodules

--- dep/build_dep.bat
+++ dep/build_dep.bat
@@ -2,14 +2,20 @@
 set VS_PATH=C:\Program Files (x86)\MSBuild\14.0\Bin\
 set PATH=%PATH%;%VS_PATH%
 
+git submodule init
+git submodule update
 if not exist lib (mkdir lib)
 
 cd log4cplus/msvc14
+git submodule init
+git submodule update
 MSBuild.exe ./log4cplusS.vcxproj /p:Configuration=Release
 copy .\Win32\bin.Release\log4cplusS.lib ..\..\lib\log4cplus.lib
 cd ../..
 
 cd protobuf/cmake
+git submodule init
+git submodule update
 cmake .
 MSBuild.exe ./libprotobuf.vcxproj /p:Configuration=Release
 copy .\Release\libprotobuf.lib ..\..\lib\protobuf.lib
