02c0ab09a63eecf26ab3d9c818572e0714763936
jinjiazhang <jinjiazh@qq.com>
2018-07-28 15:16:09 +0800

    include/copy_include.bat

---
+++ include/copy_include.bat
@@ -0,0 +1,18 @@
+if not exist lua (
+	mkdir lua
+	copy ..\dep\lua-5.3.5\src\lauxlib.h .\lua
+	copy ..\dep\lua-5.3.5\src\lua.h .\lua
+	copy ..\dep\lua-5.3.5\src\lua.hpp .\lua
+	copy ..\dep\lua-5.3.5\src\luaconf.h .\lua
+	copy ..\dep\lua-5.3.5\src\lualib.h .\lua
+)
+
+if not exist log4cplus (
+	mkdir log4cplus
+	xcopy  /E ..\dep\log4cplus\include\log4cplus .\log4cplus
+)
+
+if not exist google (
+	mkdir google
+	xcopy  /E ..\dep\protobuf\src\google .\google
+)
