3efa09da4ba7149efaec43417480d82385eb8604
jinjiazhang <jinjiazh@qq.com>
2019-04-28 23:12:59 +0800

    删除示例代码

--- bin/scripts/httpreq.lua
+++
@@ -1,8 +0,0 @@
--- http请求示例
-
-function init( ... )
-	local start = app.mstime()
-	http.async_get("http://httpbin.org/get?a=1&b=2", function( code, data )
-		log_info(string.format("http req cost %dms, code %d, data=%s", app.mstime() - start, code, data))
-	end)
-end
