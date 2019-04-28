d69de59343f88e8928b32c85e6cb3af56d400ccd
jinjiazhang <jinjiazh@qq.com>
2019-04-28 18:09:22 +0800

    修改sqlpool示例代码

--- bin/scripts/sqlpool.lua
+++ bin/scripts/sqlpool.lua
@@ -3,10 +3,10 @@ function init( ... )
 	mysql.parse("protos/db.proto")
 	pool = mysql.create_pool()
 	pool.connect("10.125.30.158", "jinjiazh", "10086", "game", 3306)
-	pool.connect("10.125.30.158", "jinjiazh", "10086", "game", 3306)
-	pool.connect("10.125.30.158", "jinjiazh", "10086", "game", 3306)
-	pool.connect("10.125.30.158", "jinjiazh", "10086", "game", 3306)
-	pool.connect("10.125.30.158", "jinjiazh", "10086", "game", 3306)
+	-- pool.connect("10.125.30.158", "jinjiazh", "10086", "game", 3306)
+	-- pool.connect("10.125.30.158", "jinjiazh", "10086", "game", 3306)
+	-- pool.connect("10.125.30.158", "jinjiazh", "10086", "game", 3306)
+	-- pool.connect("10.125.30.158", "jinjiazh", "10086", "game", 3306)
 	pool.on_respond = on_respond
 
 	-- pool.sql_create("tb_global")
@@ -16,7 +16,17 @@ function init( ... )
 	-- pool.sql_create("tb_name")
 	-- pool.sql_create("tb_online")
 	-- pool.sql_create("tb_room")
+
 	pool.sql_execute("show tables")
+	local data = {openid = "abc", roleid = 10001, svrid = 124, online = os.time()}
+	pool.sql_insert("tb_online", data)
+	pool.sql_select("tb_online", 'openid = "abc"')
+
+	data.svrid = 137
+	data.online = 0
+	pool.sql_update("tb_online", data, 'openid = "abc"')
+	pool.sql_select("tb_online", 'openid = "abc"')
+	pool.sql_delete("tb_online", 'openid = "abc"')
 end
 
 function on_respond( token, ret_code, ... )
