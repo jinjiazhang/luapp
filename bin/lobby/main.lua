f0b090958b16deab9f297dbe90f35b66e96596bb
jinjiazhang <jinjiazh@qq.com>
2019-04-28 19:44:48 +0800

    添加进程main文件

---
+++ bin/lobby/main.lua
@@ -0,0 +1,17 @@
+_G.import = app.import
+
+app.init = function ( ... )
+
+end
+
+app.proc = function ( ... )
+
+end
+
+app.tick = function ( ... )
+	app.reload()
+end
+
+app.idle = function ( ... )
+
+end
---
+++ bin/start.bat
@@ -0,0 +1,5 @@
+start luapp.exe --main-file=router/main.lua --log-conf=router/log.properties
+start luapp.exe --main-file=roomsvr/main.lua --log-conf=roomsvr/log.properties
+start luapp.exe --main-file=dbagent/main.lua --log-conf=dbagent/log.properties
+start luapp.exe --main-file=lobby/main.lua --log-conf=lobby/log.properties
+start luapp.exe --main-file=robot/main.lua --log-conf=robot/log.properties
