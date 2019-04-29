16c23af091bc42d58b5026dbff54a2f8b495e672
jinjiazhang <jinjiazh@qq.com>
2019-04-29 12:44:42 +0800

    登录帐号流程

--- bin/dbagent/login.lua
+++ bin/dbagent/login.lua
@@ -2,5 +2,32 @@
 
 function net.ss_login_req(ss, flowid, number, openid, svrid)
 	log_info("ss_login_req", ss.number, flowid, number, openid)
+	local limit = string.format("openid='%s'", openid)
+	local code, data = sqlpool.sql_select("tb_online", limit)
+	if code < 0 then
+		ss.ss_login_rsp(flowid, number, openid, -1)
+		return
+	end
+
+	if data and app.time() - data.online < 30 then
+		ss.ss_login_rsp(flowid, number, openid, data.svrid)
+		return
+	end
+
+	if not data then
+		data = { openid = openid, svrid = svrid, online = app.time() }
+		if sqlpool.sql_insert("tb_online", data) < 0 then
+			ss.ss_login_rsp(flowid, number, openid, -1)
+			return
+		end
+	else
+		-- TODO check magic
+		data.online = app.time()
+		if sqlpool.sql_update("tb_online", data, limit) < 0 then
+			ss.ss_login_rsp(flowid, number, openid, -1)
+			return
+		end
+	end
+
 	ss.ss_login_rsp(flowid, number, openid, svrid)
 end
