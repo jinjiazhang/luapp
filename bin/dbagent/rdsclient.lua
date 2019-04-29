6bfba052e048648356352d9fa7827e3574a13dd4
jinjiazhang <jinjiazh@qq.com>
2019-04-29 12:08:01 +0800

    rdsclient支持协程

--- bin/dbagent/rdsclient.lua
+++ bin/dbagent/rdsclient.lua
@@ -1,6 +1,10 @@
 -- redis连接实例
+module = "rdsclient"
+
+rdsclient_callbacks = rdsclient_callbacks or {}
 
 function init( ... )
+	setmetatable(env, {__index = __index})
 	client = redis.connect("10.125.30.158", 6379)
 	client.on_connect = on_connect
 	client.on_disconnect = on_disconnect
@@ -15,6 +19,42 @@ function on_disconnect( status, errmsg )
 	log_info("rdsclient.on_disconnect", status, errmsg)
 end
 
-function on_reply( token, status, data )
-	log_info("rdsclient.on_reply", token, status, data)
+function on_reply( token, ret_code, ... )
+	log_info("rdsclient.on_reply", token, ret_code, ...)
+	local proc_func = rdsclient_callbacks[token]
+	if proc_func then
+		rdsclient_callbacks[token] = nil
+		local status, errmsg = xpcall(proc_func, debug.traceback, ret_code, ...)
+		if not status then
+			log_error("rdsclient.on_reply xpcall fail", errmsg)
+		end
+	end
+end
+
+function __index( env, key )
+	if _G[key] then
+		return _G[key]
+	elseif type(client[key]) == 'function' then
+		env[key] = create_co_func(key, client[key])
+		return env[key]
+	end
+end
+
+function create_co_func( name, pool_func )
+	return function(...)
+		local token = pool_func(...)
+		if not token then
+			log_error("rdsclient.co_func call fail", name, ...)
+			return false
+		end
+
+		local co = coroutine.running()
+		rdsclient_callbacks[token] = function(...)
+			local status, errmsg = coroutine.resume(co, ...)
+			if not status then
+				log_error("rdsclient.co_func resume fail", name, errmsg)
+			end
+		end
+		return coroutine.yield("EXIT")
+	end
 end
