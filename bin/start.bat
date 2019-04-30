fd402e3951b4d956c8debe8e107f53caecfe2add
jinjiazhang <jinjiazh@qq.com>
2019-05-01 01:35:13 +0800

    调整日志配置目录

--- bin/start.bat
+++ bin/start.bat
@@ -1,6 +1,6 @@
-start luapp.exe --id=1.1.1.1 --main-file=router/main.lua --log-conf=router/log.properties
-start luapp.exe --id=1.1.2.1 --main-file=dbagent/main.lua --log-conf=dbagent/log.properties
-start luapp.exe --id=1.1.3.1 --main-file=listsvr/main.lua --log-conf=listsvr/log.properties
-start luapp.exe --id=1.1.4.1 --main-file=roomsvr/main.lua --log-conf=roomsvr/log.properties
-start luapp.exe --id=1.1.5.1 --main-file=lobby/main.lua --log-conf=lobby/log.properties
-rem start luapp.exe --id=1.1.9.1 --main-file=robot/main.lua --log-conf=robot/log.properties
+start luapp.exe --id=1.1.1.1 --main-file=router/main.lua --log-conf=config/router.properties
+start luapp.exe --id=1.1.2.1 --main-file=dbagent/main.lua --log-conf=config/dbagent.properties
+start luapp.exe --id=1.1.3.1 --main-file=listsvr/main.lua --log-conf=config/listsvr.properties
+start luapp.exe --id=1.1.4.1 --main-file=roomsvr/main.lua --log-conf=config/roomsvr.properties
+start luapp.exe --id=1.1.5.1 --main-file=lobby/main.lua --log-conf=config/lobby.properties
+rem start luapp.exe --id=1.1.9.1 --main-file=robot/main.lua --log-conf=config/robot.properties
