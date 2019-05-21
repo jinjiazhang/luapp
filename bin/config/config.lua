281966cee38fe3bc14712802d23236c67e4e2b11
jinjiazhang <jinjiazh@qq.com>
2019-05-21 10:43:37 +0800

    修复监听端口配置

--- bin/config/config.lua
+++ bin/config/config.lua
@@ -11,13 +11,13 @@ mysql_pass = "10086"
 mysql_db = "game"
 
 dbagent_ip = "127.0.0.1"
-dbagent_port = 8086
+dbagent_port = 6571
 
 router_ip = "127.0.0.1"
-router_port = 8087
+router_port = 6572
 
-lobby_ip = "127.0.0.1"
-lobby_port = 8088
+lobby_ip = "0.0.0.0"
+lobby_port = 6573
 
 support_mode = 1
 
