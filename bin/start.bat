79a532db98a92cae278b860d8e02c35ff46a04ff
jinjiazhang <jinjiazh@qq.com>
2020-12-31 09:38:44 +0800

    修改日志打印配置，区分windows、linux

--- bin/start.bat
+++ bin/start.bat
@@ -1,7 +1,7 @@
-start luapp.exe --id=1.1.1.1 --main-file=router/main.lua --log-conf=config/router.properties
-start luapp.exe --id=1.1.2.1 --main-file=dbagent/main.lua --log-conf=config/dbagent.properties
-start luapp.exe --id=1.1.3.1 --main-file=listsvr/main.lua --log-conf=config/listsvr.properties
-start luapp.exe --id=1.1.4.1 --main-file=gamesvr/main.lua --log-conf=config/gamesvr.properties
-start luapp.exe --id=1.1.5.1 --main-file=lobby/main.lua --log-conf=config/lobby.properties
-start luapp.exe --id=1.1.6.1 --main-file=gateway/main.lua --log-conf=config/gateway.properties
-rem start luapp.exe --id=1.1.9.1 --main-file=robot/main.lua --log-conf=config/robot.properties
+start luapp.exe --id=1.1.1.1 --main-file=router/main.lua --log-conf=config/router-win.properties
+start luapp.exe --id=1.1.2.1 --main-file=dbagent/main.lua --log-conf=config/dbagent-win.properties
+start luapp.exe --id=1.1.3.1 --main-file=listsvr/main.lua --log-conf=config/listsvr-win.properties
+start luapp.exe --id=1.1.4.1 --main-file=gamesvr/main.lua --log-conf=config/gamesvr-win.properties
+start luapp.exe --id=1.1.5.1 --main-file=lobby/main.lua --log-conf=config/lobby-win.properties
+start luapp.exe --id=1.1.6.1 --main-file=gateway/main.lua --log-conf=config/gateway-win.properties
+rem start luapp.exe --id=1.1.9.1 --main-file=robot/main.lua --log-conf=config/robot-win.properties
--- bin/start.sh
+++ bin/start.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-./luapp --id=1.1.1.1 --main-file=router/main.lua --log-conf=config/router.properties --daemon
-./luapp --id=1.1.2.1 --main-file=dbagent/main.lua --log-conf=config/dbagent.properties --daemon
-./luapp --id=1.1.3.1 --main-file=listsvr/main.lua --log-conf=config/listsvr.properties --daemon
-./luapp --id=1.1.4.1 --main-file=gamesvr/main.lua --log-conf=config/gamesvr.properties --daemon
-./luapp --id=1.1.5.1 --main-file=lobby/main.lua --log-conf=config/lobby.properties --daemon
-./luapp --id=1.1.6.1 --main-file=gateway/main.lua --log-conf=config/gateway.properties --daemon
-# ./luapp --id=1.1.9.1 --main-file=robot/main.lua --log-conf=config/robot.properties
+./luapp --id=1.1.1.1 --main-file=router/main.lua --log-conf=config/router-linux.properties --daemon
+./luapp --id=1.1.2.1 --main-file=dbagent/main.lua --log-conf=config/dbagent-linux.properties --daemon
+./luapp --id=1.1.3.1 --main-file=listsvr/main.lua --log-conf=config/listsvr-linux.properties --daemon
+./luapp --id=1.1.4.1 --main-file=gamesvr/main.lua --log-conf=config/gamesvr-linux.properties --daemon
+./luapp --id=1.1.5.1 --main-file=lobby/main.lua --log-conf=config/lobby-linux.properties --daemon
+./luapp --id=1.1.6.1 --main-file=gateway/main.lua --log-conf=config/gateway-linux.properties --daemon
+# ./luapp --id=1.1.9.1 --main-file=robot/main.lua --log-conf=config/robot-linux.properties
 
