d14fb70bb97dff8a12567289e910d67057bc876a
jinjiazhang <jinjiazh@qq.com>
2021-01-18 17:37:29 +0800

    修改日志配置文件

--- bin/start.sh
+++ bin/start.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-./luapp --id=1.1.1.1 --main-file=router/main.lua --log-conf=config/router-linux.properties --daemon
-./luapp --id=1.1.2.1 --main-file=dbagent/main.lua --log-conf=config/dbagent-linux.properties --daemon
-./luapp --id=1.1.3.1 --main-file=listsvr/main.lua --log-conf=config/listsvr-linux.properties --daemon
-./luapp --id=1.1.4.1 --main-file=gamesvr/main.lua --log-conf=config/gamesvr-linux.properties --daemon
-./luapp --id=1.1.5.1 --main-file=gateway/main.lua --log-conf=config/gateway-linux.properties --daemon
-./luapp --id=1.1.6.1 --main-file=lobby/main.lua --log-conf=config/lobby-linux.properties --daemon
-# ./luapp --id=1.1.9.1 --main-file=robot/main.lua --log-conf=config/robot-linux.properties
+./luapp --id=1.1.1.1 --name=router --main-file=router/main.lua --log-conf=config/log4cplus.properties --daemon
+./luapp --id=1.1.2.1 --name=dbagent --main-file=dbagent/main.lua --log-conf=config/log4cplus.properties --daemon
+./luapp --id=1.1.3.1 --name=listsvr --main-file=listsvr/main.lua --log-conf=config/log4cplus.properties --daemon
+./luapp --id=1.1.4.1 --name=gamesvr --main-file=gamesvr/main.lua --log-conf=config/log4cplus.properties --daemon
+./luapp --id=1.1.5.1 --name=gateway --main-file=gateway/main.lua --log-conf=config/log4cplus.properties --daemon
+./luapp --id=1.1.6.1 --name=lobby --main-file=lobby/main.lua --log-conf=config/log4cplus.properties --daemon
+# ./luapp --id=1.1.9.1 --name=robot --main-file=robot/main.lua --log-conf=config/log4cplus.properties
 
