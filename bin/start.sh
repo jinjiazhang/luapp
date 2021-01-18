42952e92a0fe2fff77e851d36477e6c975109e7f
jinjiazhang <jinjiazh@qq.com>
2021-01-18 18:09:13 +0800

    linux build

--- bin/start.sh
+++ bin/start.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-./luapp --id=1.1.1.1 --name=router --main-file=router/main.lua --log-conf=config/log4cplus.properties --daemon
-./luapp --id=1.1.2.1 --name=dbagent --main-file=dbagent/main.lua --log-conf=config/log4cplus.properties --daemon
-./luapp --id=1.1.3.1 --name=listsvr --main-file=listsvr/main.lua --log-conf=config/log4cplus.properties --daemon
-./luapp --id=1.1.4.1 --name=gamesvr --main-file=gamesvr/main.lua --log-conf=config/log4cplus.properties --daemon
-./luapp --id=1.1.5.1 --name=gateway --main-file=gateway/main.lua --log-conf=config/log4cplus.properties --daemon
-./luapp --id=1.1.6.1 --name=lobby --main-file=lobby/main.lua --log-conf=config/log4cplus.properties --daemon
-# ./luapp --id=1.1.9.1 --name=robot --main-file=robot/main.lua --log-conf=config/log4cplus.properties
+./luapp --id=1.1.1.1 --name=router --main-file=router/main.lua --log-conf=log4cplus.properties --daemon
+./luapp --id=1.1.2.1 --name=dbagent --main-file=dbagent/main.lua --log-conf=log4cplus.properties --daemon
+./luapp --id=1.1.3.1 --name=listsvr --main-file=listsvr/main.lua --log-conf=log4cplus.properties --daemon
+./luapp --id=1.1.4.1 --name=gamesvr --main-file=gamesvr/main.lua --log-conf=log4cplus.properties --daemon
+./luapp --id=1.1.5.1 --name=gateway --main-file=gateway/main.lua --log-conf=log4cplus.properties --daemon
+./luapp --id=1.1.6.1 --name=lobby --main-file=lobby/main.lua --log-conf=log4cplus.properties --daemon
+# ./luapp --id=1.1.9.1 --name=robot --main-file=robot/main.lua --log-conf=log4cplus.properties
 
